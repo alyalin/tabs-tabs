0ad9b1df3fc4929359c25fda77bf1dedc905613d
Alexander <occvlt@yandex.ru>
2019-10-11 11:01:08 +0400

    service encapsulated in module

--- app/tabs/tabs.module.ts
+++ app/tabs/tabs.module.ts
@@ -6,12 +6,14 @@ import { Tab } from "./directives/tab.directive";
 import { TabTitle } from "./directives/tab-title.directive";
 
 import { SanitizeHtmlPipe } from "./pipes/sanitize-html.pipe";
+import { TabIdService } from './services/tab-id.service';
 
 const COMPONENTS = [TabsComponent, Tab, TabContent, TabTitle];
 
 @NgModule({
   imports: [CommonModule],
   declarations: [COMPONENTS, SanitizeHtmlPipe],
+  providers: [ TabIdService ],
   exports: [COMPONENTS]
 })
 export class TabsModule {}
